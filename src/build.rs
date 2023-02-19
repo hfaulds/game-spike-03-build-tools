4fec0b6ee4173ef05e92097d169cc9078a3a2132
Hayden Faulds <fauldsh@gmail.com>
2023-02-19 02:57:20 +0000

    spawn contiguous walls

--- src/build.rs
+++ src/build.rs
@@ -5,6 +5,7 @@ use bevy::{
     render::mesh::shape::{Circle, Quad},
     sprite::MaterialMesh2dBundle,
 };
+use std::cmp::{max, min};
 use std::f32::consts::PI;
 
 #[derive(Component)]
@@ -24,13 +25,20 @@ struct WallStart {
     pos: Vec2,
 }
 
+struct WallCreateEvent {
+    from: Vec2,
+    to: Vec2,
+}
+
 pub struct BuildPlugin;
 
 impl Plugin for BuildPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup_ship_chair);
-        app.add_system(ship_wall_system);
-        app.insert_resource(WallStart::default());
+        app.add_event::<WallCreateEvent>()
+            .add_startup_system(setup_ship_chair)
+            .add_system(place_wall_system)
+            .add_system(spawn_wall_system)
+            .insert_resource(WallStart::default());
     }
 }
 
@@ -65,61 +73,87 @@ fn setup_ship_chair(
         });
 }
 
-fn ship_wall_system(
-    mut commands: Commands,
+fn place_wall_system(
     windows: Res<Windows>,
     camera: Query<(&Camera, &GlobalTransform)>,
     buttons: Res<Input<MouseButton>>,
     mut wall_start: ResMut<WallStart>,
-    ships: Query<Entity, With<Ship>>,
-
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
+    mut events: EventWriter<WallCreateEvent>,
 ) {
     if !buttons.pressed(MouseButton::Left) {
         return;
     }
 
     if let Some(cursor_global) = get_cursor_position(windows, camera) {
-        let cursor = round_to_grid(cursor_global, GRID_SIZE);
+        let to = round_to_grid(cursor_global, GRID_SIZE);
 
         if buttons.just_pressed(MouseButton::Left) {
-            wall_start.pos = cursor;
+            wall_start.pos = to;
         }
 
-        if cursor != wall_start.pos {
-            println!("from: {} to: {}", wall_start.pos, cursor);
-            let mut transform = Transform::from_translation(cursor.extend(0.));
-            if cursor.x == wall_start.pos.x {
-                transform.rotate_z(PI / 2.);
-            }
-            if cursor.x > wall_start.pos.x {
-                transform.translation += Vec3::new(-GRID_SIZE / 2., 0., 0.);
-            }
-            if cursor.x < wall_start.pos.x {
-                transform.translation += Vec3::new(GRID_SIZE / 2., 0., 0.);
-            }
-            if cursor.y > wall_start.pos.y {
-                transform.translation += Vec3::new(0., -GRID_SIZE / 2., 0.);
-            }
-            if cursor.y < wall_start.pos.y {
-                transform.translation += Vec3::new(0., GRID_SIZE / 2., 0.);
-            }
-            let ship = ships.single();
-            commands.get_entity(ship).unwrap().add_children(|parent| {
-                parent.spawn((
-                    ShipWall {},
-                    MaterialMesh2dBundle {
-                        mesh: meshes
-                            .add(Quad::new(Vec2::new(GRID_SIZE, 2.)).into())
-                            .into(),
-                        material: materials.add(ColorMaterial::from(Color::WHITE)),
-                        transform: transform,
-                        ..default()
-                    },
-                ));
-            });
-            wall_start.pos = cursor;
+        let from = wall_start.pos;
+        if from == to {
+            return;
         }
+
+        for x in (min(from.x as i32, to.x as i32)..max(from.x as i32, to.x as i32))
+            .step_by(GRID_SIZE as usize)
+        {
+            let a = Vec2::new(x as f32, from.y);
+            let b = Vec2::new(x as f32 + GRID_SIZE, from.y);
+            events.send(WallCreateEvent { from: a, to: b });
+        }
+        for y in (min(from.y as i32, to.y as i32)..max(from.y as i32, to.y as i32))
+            .step_by(GRID_SIZE as usize)
+        {
+            let a = Vec2::new(to.x, y as f32);
+            let b = Vec2::new(to.x, y as f32 + GRID_SIZE);
+            events.send(WallCreateEvent { from: a, to: b });
+        }
+        wall_start.pos = to;
+    }
+}
+
+fn spawn_wall_system(
+    mut commands: Commands,
+    ships: Query<Entity, With<Ship>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut events: EventReader<WallCreateEvent>,
+) {
+    for event in events.iter() {
+        let to = event.to;
+        let from = event.from;
+
+        let mut transform = Transform::from_translation(to.extend(0.));
+        if to.x == from.x {
+            transform.rotate_z(PI / 2.);
+        }
+        if to.x > from.x {
+            transform.translation += Vec3::new(-GRID_SIZE / 2., 0., 0.);
+        }
+        if to.x < from.x {
+            transform.translation += Vec3::new(GRID_SIZE / 2., 0., 0.);
+        }
+        if to.y > from.y {
+            transform.translation += Vec3::new(0., -GRID_SIZE / 2., 0.);
+        }
+        if to.y < from.y {
+            transform.translation += Vec3::new(0., GRID_SIZE / 2., 0.);
+        }
+        let ship = ships.single();
+        commands.get_entity(ship).unwrap().add_children(|parent| {
+            parent.spawn((
+                ShipWall {},
+                MaterialMesh2dBundle {
+                    mesh: meshes
+                        .add(Quad::new(Vec2::new(GRID_SIZE, 2.)).into())
+                        .into(),
+                    material: materials.add(ColorMaterial::from(Color::WHITE)),
+                    transform: transform,
+                    ..default()
+                },
+            ));
+        });
     }
 }
